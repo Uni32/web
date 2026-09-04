8546f86989728b218935ca5e31ef14b747f23134
Uni32 <91712755+Uni32@users.noreply.github.com>
2026-09-04 15:55:24 +0900

    Create userConfig.js

---
+++ userConfig.js
@@ -0,0 +1,15 @@
+const userConfig = {
+
+    "Popo": {
+        avatar: "P",
+        color: "#1c1c1e",
+        image: ""
+    },
+
+    "Kong": {
+        avatar: "K",
+        color: "#a8323a",
+        image: "https://i.imgur.com/JtUMDrp.jpeg"
+    },
+
+};
